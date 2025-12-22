a6dcac76cc2062819237b73f9f18c66968276e52
Enchilada429 <omaryahya.contact@gmail.com>
2025-12-22 15:05:52 +0000

    Add create_account function to database.py

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
+"""Database functions for """
+
 from os import environ as ENV, _Environ
 
+from re import match
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -13,13 +16,27 @@ def get_mongodb_client(config: _Environ) -> MongoClient:
     )
 
 
+def create_account(mongodb_client: MongoClient, username: str, password: str, email: str) -> None:
+    """Create a HabitQuest account with username, password, and email.
+    Returns error if email has invalid format."""
+
+    if not match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", email):
+        raise ValueError("Email has invalid format.")
+
+    account_collection = mongodb_client["HabitQuest"]["account"]
+
+    account_collection.insert_one({
+        "username": username,
+        "password": password,
+        "email": email
+    })
+
+
 if __name__ == "__main__":
     load_dotenv()
 
-    # Send a ping to confirm a successful connection
-    try:
-        client = get_mongodb_client(ENV)
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
+    client = get_mongodb_client(ENV)
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+
+    create_account(client, "omar", "yahya", "example@gmail.com")
